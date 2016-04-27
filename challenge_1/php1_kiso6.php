b8f4f52ae33dfeb74ce6dbe1f227c7c9c05d030c
runamurakami <ru.aug02.xxx@gmail.com>
2016-04-27 14:34:25 +0900

    課題コミット

---
+++ challenge_1/php1_kiso6.php
@@ -0,0 +1,14 @@
+<?php
+   $num1 = "e";
+
+   if($num1 == 1){
+     echo "「1です！」";
+   }elseif($num1 == 2){
+     echo "「プログラミングキャンプ！」";
+   }elseif($num1 == 'a'){
+     echo "「文字です！」";
+   }else{
+     echo "「その他です！」";
+   }
+
+?>
